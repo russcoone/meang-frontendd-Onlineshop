cbca5fd7e9502669b922388754b6f01be61d2e82
russcoone <russcoone@MacBook-Pro-de-russcoone.local>
2022-12-19 12:24:18 -0600

    volvimos aponer un commit porque en otro punto nolo ponia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./@public/pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'contact',
+    loadChildren: () =>
+      import('./@public/pages/contact/contact.module').then(
+        (m) => m.ContactModule
+      ),
+  },
 
-const routes: Routes = [];
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
+import { PublicModule } from './@public/pages/public.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminModule } from './@admin/pages/admin.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AdminModule, PublicModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
