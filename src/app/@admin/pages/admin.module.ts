76c02f45b61bebffb37e52a87806ad5f04f85cf1
russcoone <russcoone@MacBook-Pro-de-russcoone.local>
2022-12-29 12:54:10 -0600

    conversion de la platilla simple siderbar startboostrap en al plantilla del apartado del proyecto

--- src/app/@admin/pages/admin.module.ts
+++ src/app/@admin/pages/admin.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-
+import { HeaderComponent } from '../core/components/header/header.component';
+import { TitleComponent } from '../core/components/title/title.component';
+import { SidebarComponent } from '../core/components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AdminComponent],
-  imports: [
-    CommonModule,
-    AdminRoutingModule
-  ]
+  declarations: [AdminComponent, HeaderComponent, TitleComponent, SidebarComponent],
+  imports: [CommonModule, AdminRoutingModule],
 })
-export class AdminModule { }
+export class AdminModule {}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () =>
-      import('./@public/pages/home/home.module').then((m) => m.HomeModule),
-  },
-  {
-    path: 'contact',
-    loadChildren: () =>
-      import('./@public/pages/contact/contact.module').then(
-        (m) => m.ContactModule
-      ),
-  },
-
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
