fbb024f60fe38700a8ae5d133c7aebf156f508f2
Abraham casas <abrahamcomservice@gmail.com>
2023-01-25 22:28:03 -0600

    03-plantilla base de la tienda terminada

--- src/app/@admin/pages/admin.module.ts
+++ src/app/@admin/pages/admin.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-import { HeaderComponent } from '../core/components/header/header.component';
-import { TitleComponent } from '../core/components/title/title.component';
-import { SidebarComponent } from '../core/components/sidebar/sidebar.component';
+import { HeaderComponent } from '@admin-core/components/header/header.component';
+import { TitleComponent } from '@admin-core/components/title/title.component';
+import { SidebarComponent } from '@admin-core/components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AdminComponent, HeaderComponent, TitleComponent, SidebarComponent],
+  declarations: [
+    AdminComponent,
+    HeaderComponent,
+    TitleComponent,
+    SidebarComponent,
+  ],
   imports: [CommonModule, AdminRoutingModule],
 })
 export class AdminModule {}
